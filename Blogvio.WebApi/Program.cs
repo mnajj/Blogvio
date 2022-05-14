56259c340e7ad2f4d449b84a3ff32b13bd21ad12
Mohab Alnajjar <mohabsoft21@outlook.com>
2022-05-14 18:20:11 +0200

    Register sql server

--- Blogvio.WebApi/Program.cs
+++ Blogvio.WebApi/Program.cs
@@ -1,6 +1,17 @@
+using Blogvio.WebApi.Data;
+using Blogvio.WebApi.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//
+builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
