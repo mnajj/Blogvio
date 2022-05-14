e7b8d2d6022b5c6ed0cd40fae726669f5bc00667
Mohab Alnajjar <mohabsoft21@outlook.com>
2022-05-14 18:07:43 +0200

    Add Db Context

---
+++ Blogvio.WebApi/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Blogvio.WebApi.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blogvio.WebApi.Data
+{
+	public class AppDbContext : IdentityDbContext<AppUser>
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+	}
+}
---
+++ Blogvio.WebApi/Models/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blogvio.WebApi.Models
+{
+	public class AppUser : IdentityUser
+	{
+		[Required, MaxLength(50)]
+		public string FirstName { get; set; }
+
+		[Required, MaxLength(50)]
+		public string LastName { get; set; }
+	}
+}
