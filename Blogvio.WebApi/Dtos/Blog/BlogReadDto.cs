03f7083cf083b36b8a1da3064e32238a9da34fc3
Mohab Alnajjar <mohabsoft21@outlook.com>
2022-05-16 15:43:20 +0200

    add blog dtos

---
+++ Blogvio.WebApi/Dtos/Blog/BlogReadDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blogvio.WebApi.Dtos.Blog
+{
+	public class BlogReadDto
+	{
+		[Required]
+		public int Id { get; set; }
+
+		[Required]
+		public string Url { get; set; }
+
+		[Required]
+		public DateTime CreatedAt { get; set; }
+	}
+}
---
+++ Blogvio.WebApi/Dtos/Blog/BlogUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blogvio.WebApi.Dtos.Blog
+{
+	public class BlogUpdateDto
+	{
+		[Required]
+		public string Url { get; set; }
+	}
+}
