491438511592f54025cdd0edb6cb20321dd26ab4
Mohab Alnajjar <mohabsoft21@outlook.com>
2022-05-18 15:26:45 +0200

    Add Post Repo

---
+++ Blogvio.WebApi/Repositories/IPostRepository.cs
@@ -0,0 +1,14 @@
+﻿using Blogvio.WebApi.Models;
+
+namespace Blogvio.WebApi.Repositories
+{
+	public interface IPostRepository
+	{
+		Task<IEnumerable<Post>> GetPostsAsync();
+		Task<Post> GetPostByIdAsync(int id);
+		Task CreatePostAsync(Post post);
+		Task UpdatePostAsync(Post post);
+		Task DeletePostAsync(int id);
+		Task<bool> SaveChanges();
+	}
+}
---
+++ Blogvio.WebApi/Repositories/PostRepository.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Blogvio.WebApi.Data;
+using Blogvio.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blogvio.WebApi.Repositories
+{
+	public class PostRepository : IPostRepository
+	{
+		private readonly AppDbContext _context;
+		private readonly IMapper _mapper;
+
+		public PostRepository(AppDbContext context, IMapper mapper)
+		{
+			_context = context;
+			_mapper = mapper;
+		}
+		public async Task CreatePostAsync(Post post)
+		{
+			await _context.Posts.AddAsync(post);
+		}
+
+		public async Task DeletePostAsync(int id)
+		{
+			var post = await GetPostByIdAsync(id);
+			post.IsDeleted = true;
+		}
+
+		public async Task<Post> GetPostByIdAsync(int id)
+		{
+			return await _context
+				.Posts
+				.FirstOrDefaultAsync(p => p.Id == id);
+		}
+
+		public async Task<IEnumerable<Post>> GetPostsAsync()
+		{
+			return await _context.Posts.ToListAsync();
+		}
+
+		public async Task<bool> SaveChanges()
+		{
+			return (await _context.SaveChangesAsync() >= 0);
+		}
+
+		public async Task UpdatePostAsync(Post post)
+		{
+			_context.Entry(
+				await _context.Posts
+					.FirstOrDefaultAsync(p => p.Id == post.Id)
+				)
+				.CurrentValues
+				.SetValues(post);
+		}
+	}
+}
